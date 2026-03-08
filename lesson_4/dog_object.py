5f9ec471fc09905de69d2e46d0ed6180beb299ec
efrat <efratn2021@gmail.com>
2026-03-08 18:55:34 +0200

    update python file

---
+++ lesson_4/dog_object.py
@@ -0,0 +1,20 @@
+# class DogObject():
+#     def __init__(self,name,age):
+#        print('into dog costructor')
+#         self.name = name
+#         self.age = age
+#
+#     def go_to_sleep(self, time_to_sleep):
+#         print(f'going to sleep for {time_to_sleep} seconds')
+#
+#     def make_noise(self):
+#         print('ho ho ho ho')
+#
+#     def calcukate_distance(self, time, speed):
+#         distance = time * speed
+#         if distance < 10:
+#             print('the dog is lazzy')
+#         else:
+#             print('the dog is ok')
+#
+#
---
+++ lesson_4/main_animals.py
@@ -0,0 +1,6 @@
+from lesson_4.dog_object import DogObject
+
+
+class MainAnimal:
+   dog_1 = DogObject('Rexy',4)
+   dog_2 = DogObject('lucky',6)
